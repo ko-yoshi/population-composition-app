f96a562ce68dc94c0c47a0bdde45553045fbb40f
koji.yoshikawa <koji.yoshikawa@adglobe.co.jp>
2023-04-13 17:38:42 +0900

    チェックボックスボタンコンポーネント作成

---
+++ src/components/Parts/CheckboxBtn.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="checkbox-btn">
+    <label :class="{ checked: model }">
+      <input v-model="model" type="checkbox" @change="emits('change', model)" />
+      {{ props.label }}
+    </label>
+  </div>
+</template>
+<script lang="ts">
+  type Props = { label: string; checked?: boolean }
+  type Emits = { (e: 'change', val: boolean): void }
+</script>
+<script setup lang="ts">
+  const props = withDefaults(defineProps<Props>(), {
+    label: '',
+    checked: false,
+  })
+  const emits = defineEmits<Emits>()
+  const model = ref(props.checked)
+</script>
+<style lang="scss" scoped>
+  .checkbox-btn {
+    position: relative;
+    display: inline-block;
+
+    input {
+      position: absolute;
+      left: 0;
+      opacity: 0;
+    }
+
+    label {
+      display: inline-block;
+      min-width: 6em;
+      padding: 0.6em;
+      font-size: 12px;
+      text-align: center;
+      cursor: pointer;
+      background-color: #eee;
+      border-radius: 5px;
+      transition: 0.2s;
+
+      &:hover,
+      &.checked {
+        color: #fff;
+        background-color: #4287f5;
+      }
+    }
+  }
+</style>
