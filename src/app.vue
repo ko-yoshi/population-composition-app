98c0a7a07f79a0bea98a397493192b1dec272f1c
koji.yoshikawa <koji.yoshikawa@adglobe.co.jp>
2023-04-16 00:06:08 +0900

    各コンポーネントを表示するように修正

--- src/app.vue
+++ src/app.vue
@@ -1,17 +1,31 @@
 <template>
-  <main class="parent">
+  <main class="main">
     <section class="header">
       <h1>都道府県別人口推移グラフ</h1>
     </section>
-    <ObjectsPrefecturesArea class="prefectures" />
+    <section>
+      <ObjectsPrefecturesArea />
+    </section>
+    <section class="population-type-area">
+      <ObjectsPopulationTypeArea />
+    </section>
+    <section class="line-chart-area">
+      <ObjectsLineChart />
+    </section>
   </main>
 </template>
 <script setup></script>
 <style lang="scss" scoped>
-  main {
+  .main {
     display: flex;
     flex-direction: column;
     width: 100vw;
     height: 100vh;
+    padding: 1.5em;
+
+    .population-type-area,
+    .line-chart-area {
+      margin-top: 1em;
+    }
   }
 </style>
